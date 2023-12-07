3fef370000c662a157dd67311d8f92faab69cb83
antonio oliveira <antoniofmoliveira@outlook.com>
2023-12-07 08:39:34 -0300

    add page for customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,28 @@
 import { Metadata } from 'next';
+import Table from '@/app/ui/customers/table';
+import { CustomersTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
     title: 'Customers',
 };
 
-export default function Page() {
-    return <p>Customers Page</p>;
-  }
+export default async function Page({
+    searchParams,
+}: {
+    searchParams?: {
+        query?: string;
+    };
+}) {
+    const query = searchParams?.query || '';
+
+    return (
+        <div className="w-full">
+
+            <Suspense key={query} fallback={<CustomersTableSkeleton />}>
+                <Table query={query} />
+            </Suspense>
+
+        </div>
+    );
+}
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -250,3 +250,4 @@ export async function getUser(email: string) {
         throw new Error('Failed to fetch user.');
     }
 }
+
